8a04b829dd897596da5089f61dd14fac406bdf00
Andrei <git1@email.com>
2024-11-05 15:56:05 +0100

    Counter app - Simple counter app

--- counter-app/src/App.jsx
+++ counter-app/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -8,26 +6,18 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <h1>Counter App</h1>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className="card">
+        <h3>Value</h3>
+        <h4>count is {count}</h4>
+      </div>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>Add</button>
+        <button onClick={() => setCount((count) => count - 1)}>Decrease</button>
+        <button onClick={() => setCount(0)}>Reset</button>
+      </div>
     </>
   )
 }
